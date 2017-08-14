9fb177f830d2de1b4cbfcf60290708d14dd3bf9d
fernsberns6 <jf.berns@gmail.com>
2017-08-13 20:23:37 -0700

    commit 2

--- server.js
+++ server.js
@@ -92,9 +92,9 @@ app.get('/listofusers/:id',function(req,res){
 //		});
 //});
 
+var port = process.env.PORT || 3000;
 
-
-var server = app.listen(3000,function(){
+var server = app.listen(port, function() {
 console.log('Chat server running on port 3000');
 });
 
